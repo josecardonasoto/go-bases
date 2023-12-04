d02109b3ea47045c242b3941c6da7707d8b5067f
Jose David <jose.cardonasoto@mercadolibre.com.co>
2023-12-04 13:31:46 -0500

    feat(Practica 1 - Introducción a Go): Exercises of practice 1 added

---
+++ Práctica 1 - Introducción al lenguaje GO/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// EJERCICIO 1
+	fmt.Println("------- EJERCICIO 1 -------")
+	var nombre string = "Jose David"
+	var direccion string = "Medellín, Colombia"
+	fmt.Println("Nombre:", nombre)
+	fmt.Println("Dirección:", direccion)
+
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("")
+
+	// EJERCICIO 2
+	fmt.Println("------- EJERCICIO 2 -------")
+	var temperatura int32 = 32
+	var humedad int32 = 10
+	var presion int32 = 100
+	fmt.Println("Temperatura:", temperatura)
+	fmt.Println("Humedad:", humedad)
+	fmt.Println("Presión:", presion)
+
+}
